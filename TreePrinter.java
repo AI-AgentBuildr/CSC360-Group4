3eb9490fadfa1bf95e44977592d87a8ff0ab34a8
Dev Patel <devpatel@Devs-MacBook-Pro.local>
2026-09-17 14:41:12 +0530

    Day 2: Add TreePrinter shell

---
+++ TreePrinter.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class TreePrinter {
+    public static void printTree(TreeNode root) {
+        System.out.println(root.name);
+        // full logic added Day 3, once StackFrame/TreeTraversal exist
+    }
+}
